5c36c0aaa0e89ddef069c8687705501f9a8d6f47
Yorguin Murillo <ymurillo2@ncstudents.niagaracollege.ca>
2020-07-10 23:20:07 -0400

    useState, useEffect

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+const NoteApp = () => {
+  const [notes, setNotes] = useState([])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addNote = (e) => {
+    e.preventDefault()
+    setNotes([
+      ...notes,
+      { title, body }
+    ])
+    setTitle('')
+    setBody('')
+  }
+
+  const removeNote = (title) => {
+    setNotes(notes.filter((note) => note.title !== title ))
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      {notes.map((note) => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+          <button onClick={() => removeNote(note.title)}> X </button>
+        </div>
+      ))}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)}  placeholder="Title"/>
+        <input value={body} onChange={(e) => setBody(e.target.value)}  placeholder="Body"/>
+        <button> Add Note </button>
+      </form>
+    </div>
+  )
+}
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NoteApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +56,34 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+
+
+
+
+// const App = (props) => {
+//   const [ count, setCount ] = useState(props.count)
+//   const [ text, setText ] = useState('')
+
+//   const increment = () => {
+//     setCount(count + 1)
+//   }
+
+//   const decrement =  () => {
+//     setCount(count - 1)
+//   }
+
+//   return (
+//     <div>
+//      <p> The current { text || 'count' } is { count } </p>
+//      <button onClick={increment} > +1 </button>
+//      <button onClick={decrement} > -1 </button>
+//      <button onClick={() => setCount(0)} > Reset </button>
+//      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+
+//     </div>
+//   )
+// }
